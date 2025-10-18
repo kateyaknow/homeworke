839c5d63f4f862483f1e33c9ca637a21a4eeb047
kateyaknow <kateee8eee@gmail.com>
2025-10-18 19:49:12 +0200

    Create Task6.py

---
+++ tests/Task6.py
@@ -0,0 +1,12 @@
+OK_FORMAT = True
+
+test = {   'name': 'Task6',
+    'points': 2,
+    'suites': [   {   'cases': [   {   'code': '>>> assert np.allclose(StandardScaler().fit_transform(np.array([[2.7], [2.9], [2.2], [2.0]])), np.array([[0.687], [1.236], [-0.687], [-1.236]]), '
+                                               'atol=0.001)\n',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
