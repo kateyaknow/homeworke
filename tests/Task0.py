348b660b7b673b211c08157ba95b56522780e53f
kateyaknow <kateee8eee@gmail.com>
2025-10-18 19:47:16 +0200

    Create Task0.py

---
+++ tests/Task0.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'Task0',
+    'points': 0,
+    'suites': [{'cases': [{'code': '>>> assert data.shape == (5000, 8)\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
